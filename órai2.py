01a865e594dea4b3966affb69592acf966216ecf
Tanuló 301-11 <301-11@KKSZKI.LOCAL>
2021-11-30 18:31:17 +0100

    Második feladat

--- órai2.py
+++ órai2.py
@@ -4,3 +4,9 @@ for szam in range(1,11,1):
     reciprok=1/szam
     print(reciprok)
 
+#2. írj programot, mely beolvassa a hatvány alapját és a kitevőt, és kiírja a hatványértéket!
+
+alap=int(input("Adja meg a hatvány alapját: "))
+kitevo=int(input("Adja meg a hatványkitevőt: "))
+hatvanyertek=alap**kitevo
+print("A hatványérték: {0}" .format(hatvanyertek))
