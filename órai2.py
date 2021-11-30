9fe1e5c282b841a42d6d0a4602cf2f1b0ecaacae
Tanuló 301-11 <301-11@KKSZKI.LOCAL>
2021-11-30 18:21:41 +0100

    Első feladat

--- órai2.py
+++ órai2.py
@@ -1,2 +1,6 @@
-print ("Git beállítása")
-print ("Ez a folytatás!!")
+#1. Írjon programot, mely kiírja a képernyőre a 1-10 ig a számok reciprokát!
+
+for szam in range(1,11,1):
+    reciprok=1/szam
+    print(reciprok)
+
