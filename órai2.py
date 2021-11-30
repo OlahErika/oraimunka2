23f4f94b3ca0fa36d2e56207ef0968a1285c2570
Tanuló 301-11 <301-11@KKSZKI.LOCAL>
2021-11-30 18:44:17 +0100

    Harmadik feladat

--- órai2.py
+++ órai2.py
@@ -10,3 +10,9 @@ alap=int(input("Adja meg a hatvány alapját: "))
 kitevo=int(input("Adja meg a hatványkitevőt: "))
 hatvanyertek=alap**kitevo
 print("A hatványérték: {0}" .format(hatvanyertek))
+
+#3. Írj programot, ami csak pozitív számot hajlandó beolvasni.
+
+szam2=int(input("Adjon meg egy pozitív számot! "))
+if szam2<0:
+    print("Ez a szám nem pozitív. Kérem adjon meg egy újat! ")
